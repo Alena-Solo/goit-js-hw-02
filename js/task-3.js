aac80964893b00c3a5eb600b59253e438385eda1
Alena-Solo <bazarkina.alena2020@gmail.com>
2024-09-01 09:03:00 +0200

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function checkForSpam(message) {
-  const lowerCaseMessage = message.tolowerCase();
+  const lowerCaseMessage = message.toLowerCase();
   if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
     return true;
   } else {
